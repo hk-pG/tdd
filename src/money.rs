74511bad19c9d44ba99ab8ade3879c7fefa8e9d2
hk-pG <hukada.gogotea@gmail.com>
2026-02-16 00:20:59 +0900

    fix: Money構造体を追加し、Dollar/Franc構造体からamountフィールドを削除

--- src/money.rs
+++ src/money.rs
@@ -1,16 +1,54 @@
 #[derive(PartialEq, Debug)]
+pub struct Money {
+    pub amount: i32,
+}
+
+impl Money {
+    pub fn new(amount: i32) -> Money {
+        Money { amount }
+    }
+}
+
+///
+/// 通貨構造体(Dollar)
+///
+#[derive(PartialEq, Debug)]
 pub struct Dollar {
-    amount: i32,
+    money: Money,
 }
 
 impl Dollar {
     pub fn new(amount: i32) -> Dollar {
-        Self { amount }
+        Self {
+            money: Money::new(amount),
+        }
     }
 
     pub fn times(&self, multiplier: u32) -> Dollar {
         Self {
-            amount: self.amount * multiplier as i32,
+            money: Money::new(self.money.amount * multiplier as i32),
+        }
+    }
+}
+
+///
+/// 通貨構造体(Franc)
+///
+#[derive(PartialEq, Debug)]
+pub struct Franc {
+    money: Money,
+}
+
+impl Franc {
+    pub fn new(amount: i32) -> Franc {
+        Self {
+            money: Money::new(amount),
+        }
+    }
+
+    pub fn times(&self, multiplier: u32) -> Franc {
+        Self {
+            money: Money::new(self.money.amount * multiplier as i32),
         }
     }
 }
@@ -22,6 +60,9 @@ mod tests {
     // TODO: $5 + 10 CHF = $10 (レートが2:1の場合)
     // TODO: Moneyの丸め処理をどうする？
     // TODO: 他のオブジェクトとの等価性比較
+    // TODO: DollarとFrancの重複
+    // TODO: timesの一般化
+    // TODO: DollarとFrancの比較
     #[test]
     fn test_multiplication() {
         // arrange
@@ -33,13 +74,19 @@ mod tests {
 
     #[test]
     fn test_equals() {
-        // arrange
-        let five = Dollar::new(5);
-
         // act+assert
-        assert_eq!(five, Dollar::new(5));
-        assert_ne!(five, Dollar::new(1));
-        assert_eq!(Dollar::new(0), Dollar::new(0));
-        assert_ne!(Dollar::new(1), Dollar::new(10));
+        assert_eq!(Dollar::new(5), Dollar::new(5));
+        assert_ne!(Dollar::new(5), Dollar::new(1));
+        assert_eq!(Franc::new(5), Franc::new(5));
+        assert_ne!(Franc::new(5), Franc::new(1));
+    }
+
+    #[test]
+    fn test_franc_multiplication() {
+        // arrange
+        let five = Franc::new(5);
+        // act+assert
+        assert_eq!(Franc::new(10), five.times(2));
+        assert_eq!(Franc::new(15), five.times(3));
     }
 }
