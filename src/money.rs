97deab1db06b75d6dfbfcb6c2676caa3dcaa5805
hk-pG <hukada.gogotea@gmail.com>
2026-02-15 17:40:19 +0900

    fix: 等価比較が可能なようにPartialEqを実装

--- src/money.rs
+++ src/money.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq)]
 pub struct Dollar {
     pub amount: i32,
 }
@@ -35,5 +36,28 @@ mod tests {
         let product = five.times(3);
 
         assert_eq!(15, product.amount);
+    }
+
+    #[test]
+    fn test_equals() {
+        // arrange
+        let five = Dollar::new(5);
+        let five_2 = Dollar::new(5);
+
+        // act
+        let is_equal = five == five_2;
+
+        // assert
+        assert_eq!(true, is_equal);
+
+        // arrange
+        let one = Dollar::new(1);
+        // assert
+        let is_equal = five.amount == one.amount;
+        // act
+        assert_eq!(false, is_equal);
+
+        assert_eq!(true, Dollar::new(0) == Dollar::new(0));
+        assert_eq!(false, Dollar::new(1) == Dollar::new(10));
     }
 }
