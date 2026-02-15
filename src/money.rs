92ab03d2ce191086a54f5838e7662adb046e744c
hk-pG <hukada.gogotea@gmail.com>
2026-02-15 16:02:50 +0900

    add: 1サイクルを回し、とりあえずグリーンになるように実装

---
+++ src/money.rs
@@ -0,0 +1,37 @@
+pub struct Dollar {
+    pub amount: i32,
+}
+
+impl Dollar {
+    pub fn new(amount: i32) -> Dollar {
+        Self { amount }
+    }
+
+    pub fn times(&self, multiplier: u32) -> Dollar {
+        Self {
+            amount: self.amount * multiplier as i32,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: $5 + 10 CHF = $10 (レートが2:1の場合)
+    // TODO: $5 * 2 = $10
+    // TODO: amountをプライベートにする
+    // TODO: Dollarの副作用をどうする？
+    // TODO: Moneyの丸め処理をどうする？
+    #[test]
+    fn test_multiplication() {
+        // arrange
+        let five = Dollar::new(5);
+
+        // act
+        let ten = five.times(2);
+
+        // assert
+        assert_eq!(10, ten.amount);
+    }
+}
